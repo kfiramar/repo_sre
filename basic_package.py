5032d1e23bbccc85b1fd81e554d835619b2db054
kfir <kfira2002@gmail.com>
2022-12-24 23:24:06 +0200

    added messages

--- basic_package.py
+++ basic_package.py
@@ -1,13 +1,24 @@
 '''This module contains the row class which represents a row in a CLI chart'''
 
+from collections import deque
 import hashlib
 import requests
 from config import TIMEOUT
 
 
 class BasicPackage:
-    '''This class represents a row in a CLI chart'''
+    '''
+    Initialize a Row object with the given attributes.
 
+    Parameters:
+        package_type (str): The type of package.
+        package_url (str): The URL of the package.
+        package_hash (str): The hash of the package.
+        failed_downloads_counter (int): The counter for failed downloads.
+        successful_downloads_counter (int): The counter for successful downloads.
+        request_time_summary (float): Prometheus summary of request times.
+        content (bytes, optional): The content of the package. Defaults to None.
+    '''
     def __init__(self, package_type, package_url, package_hash, failed_downloads_counter, successful_downloads_counter, request_time_summary, content=None):
         self.package_type = package_type
         self.package_url = package_url
@@ -17,11 +28,25 @@ class BasicPackage:
         self.successful_downloads_counter = successful_downloads_counter
         self.request_time_summary = request_time_summary
 
-    def prometheus_fail(self, requests_deque):
+    # Method to increase the counter for failed downloads and add 0 to the requests deque
+    def prometheus_fail(self, requests_deque: deque) -> None:
+        '''
+        Increase the counter for failed downloads and add 0 to the requests deque.
+
+        Parameters:
+            requests_deque (deque): A deque of requests.
+        '''
         requests_deque.append(0)
         self.failed_downloads_counter.inc()
 
-    def prometheus_info(self, requests_deque):
+    # Method to increase the counter for successful downloads and add 1 to the requests deque
+    def prometheus_info(self, requests_deque: deque) -> None :
+        '''
+        Increase the counter for successful downloads and add 1 to the requests deque.
+
+        Parameters:
+            requests_deque (deque): A deque of requests.
+        '''
         requests_deque.append(1)
         self.successful_downloads_counter.inc()
 
@@ -40,16 +65,22 @@ class BasicPackage:
                 request_time_summary=request_time_summary
             )
 
-    def download_package(self):
+    # Method to download the package
+    def download_package(self) -> str:
         with self.request_time_summary.time():
             try:
+                # Send a GET request to the package URL
                 response = requests.get(self.package_url, timeout=TIMEOUT)
+                # Raise exception if the request was not successful
                 response.raise_for_status()
+                # Set the package content to the response content
                 self.content = response.content
             except requests.exceptions.RequestException as exception:
                 return exception
 
     def verify_package_hash(self) -> bool:
         """Verify the hash of the given package."""
+        # Calculate the hash of the package content
         package_hash = hashlib.sha256(self.content).hexdigest()
+        # Return True if the calculated hash matches the package hash, False otherwise
         return package_hash == self.package_hash
--- main.py
+++ main.py
@@ -1,59 +1,86 @@
 import collections
 import logging
+
+# Import the BlockingScheduler class from the apscheduler library
 from apscheduler.schedulers.blocking import BlockingScheduler
+
+# Import custom classes and variables
 from basic_package import BasicPackage
-from prometheus_items import up_status, pypi_request_time, pypi_successful_downloads, pypi_failed_downloads, npm_request_time, npm_successful_downloads, npm_failed_downloads, deb_request_time, deb_successful_downloads, deb_failed_downloads
-from config import DEB_HASH, DEB_PACKAGE_URL, DEQUE_SIZE, DOWNLOAD_FAILED, INCORRECT_HASH, NPM_HASH, NPM_PACKAGE_URL, PYPI_HASH, PYPI_PACKAGE_URL, REQUEST_EVERY, SLA, SUCCESS
+from prometheus_items import (up_status, pypi_request_time, pypi_successful_downloads, pypi_failed_downloads,
+                              npm_request_time, npm_successful_downloads, npm_failed_downloads,
+                              deb_request_time, deb_successful_downloads, deb_failed_downloads)
+
+# Import constants from config file
+from config import DEB_HASH, DEB_PACKAGE_URL, DEQUE_SIZE, DOWNLOAD_FAILED, INCORRECT_HASH, \
+    NPM_HASH, NPM_PACKAGE_URL, PYPI_HASH, PYPI_PACKAGE_URL, REQUEST_EVERY, SLA, SUCCESS
 
 # Set up logging
 logger = logging.getLogger(__name__)
+# Set the logging level for the apscheduler logger to ERROR to suppress unnecessary log messages
 logging.getLogger('apscheduler').setLevel(logging.ERROR)
+# Set the logging level for the current logger to INFO
 logger.setLevel(logging.INFO)
+# Create a formatter for the log messages
 myformatter = logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s', '%H:%M:%S')
+# Create a stream handler to log messages to the console
 myhandler = logging.StreamHandler()
+# Set the formatter for the stream handler
 myhandler.setFormatter(myformatter)
+# Add the stream handler to the logger
 logger.addHandler(myhandler)
 
-# set deque
+# Create a deque to store the history of requests. It is used by the up_status variable.
 requests_deque = collections.deque(maxlen=DEQUE_SIZE)
 
 
-def update_metrics(package):
-    """Update the Prometheus metrics."""
+def process_package(package : BasicPackage) -> None:
+    """
+    Downloads the packages and checks if they are valid. 
+    If the package is valid, update prometheus with information about the package.
+    If the package is invalid, log an error and update prometheus with information about the failure.
+
+    Args:
+        package (BasicPackage): An instance of the BasicPackage class representing a package to be processed.
+    """
+    # Download the package and store exception (if exists) result in the exception variable
     exception = package.download_package()
+    # If the download was successful (i.e. no exception was raised)
     if not exception:
+        # Verify the package hash to ensure the package is valid
         if package.verify_package_hash():
             package.prometheus_info(requests_deque)
             logger.info(SUCCESS.format(package.package_url))
         else:
+            # Update prometheus with information about the failed download
             package.prometheus_fail(requests_deque)
             logger.error(INCORRECT_HASH.format(package.url))
+    # If the download was not successful (i.e. an exception was raised)
     else:
+        # Update prometheus with information about the failed download
         package.prometheus_fail(requests_deque)
         logger.error(DOWNLOAD_FAILED.format(exception))
+    # Update the up_status variable based on the success rate of recent requests
     up_status.set(SLA < (sum(requests_deque) / len(requests_deque)))
-    print(requests_deque)
 
 
-def main():
-    """Main function."""
-    # Set up scheduling
+def main() -> None:
+    """Set up scheduling and run the process_package function on a regular interval for each package type."""
+    # Create instances of the BasicPackage class for each package type
     pypi_package = BasicPackage("pypi", PYPI_PACKAGE_URL, PYPI_HASH,
                                 pypi_failed_downloads, pypi_successful_downloads, pypi_request_time)
     npm_package = BasicPackage("NPM", NPM_PACKAGE_URL, NPM_HASH,
                                npm_failed_downloads, npm_successful_downloads, npm_request_time)
     deb_package = BasicPackage("DEB", DEB_PACKAGE_URL, DEB_HASH,
                                deb_failed_downloads, deb_successful_downloads, deb_request_time)
+    # Create a blocking scheduler
     scheduler = BlockingScheduler()
-    scheduler.add_job(update_metrics, 'interval',
-                      seconds=REQUEST_EVERY, args=(pypi_package,), id='pypi_job',)
-    scheduler.add_job(update_metrics, 'interval',
+    # Add a job to the scheduler to run the process_package function on a regular interval for each package type
+    scheduler.add_job(process_package, 'interval', seconds=REQUEST_EVERY, args=(
+        pypi_package,), id='pypi_job',)
+    scheduler.add_job(process_package, 'interval',
                       seconds=REQUEST_EVERY, args=(npm_package,), id='npm_job',)
-    scheduler.add_job(update_metrics, 'interval',
+    scheduler.add_job(process_package, 'interval',
                       seconds=REQUEST_EVERY, args=(deb_package,), id='deb_job',)
+    # Start the scheduler
     scheduler.start()
-
-
-if __name__ == '__main__':
-    main()
